6913af77ed7f42d9d600caba0e5ff57274965458
tlenguyen <28319279+tlenguyen@users.noreply.github.com>
2018-03-01 08:42:53 -0800

    Contentful frontend testing tool rewritten in AngularJS.

--- app/contentfulController.js
+++ app/contentfulController.js
@@ -7,74 +7,52 @@ app.controller('contentfulController', [
 
         $scope.model.placements = [
             {
-                name: 'Homepage Container',
-                link: 'https://pp.grubhub.com/?chiri_homepage-container=',
-                id: '1'
-            },
-            {
                 name: 'Homepage Hero',
-                link: 'https://pp.grubhub.com/?chiri_homepage-hero-content=',
-                id: '2'
+                link: 'https://pp.grubhub.com/?chiri_homepage-hero-content='
             },
             {
                 name: 'Homepage Top',
-                link: 'https://pp.grubhub.com/?chiri_homepage-top-content=',
-                id: '3'
+                link: 'https://pp.grubhub.com/?chiri_homepage-top-content='
             },
             {
                 name: 'Homepage Middle',
-                link: 'https://pp.grubhub.com/?chiri_homepage-middle-content=',
-                id: '4'
+                link: 'https://pp.grubhub.com/?chiri_homepage-middle-content='
             },
             {
                 name: 'Homepage Footer',
-                link: 'https://pp.grubhub.com/?chiri_homepage-footer-content=',
-                id: '5'
+                link: 'https://pp.grubhub.com/?chiri_homepage-footer-content='
             },
             {
                 name: 'Homepage Subfooter',
-                link: 'https://pp.grubhub.com/?chiri_homepage-subfooter-content=',
-                id: '6'
-            },
-            {
-                name: 'Let\'s Eat Container',
-                link: 'https://pp.grubhub.com/lets-eat/?chiri_homepage-container=',
-                id: '7'
+                link: 'https://pp.grubhub.com/?chiri_homepage-subfooter-content='
             },
             {
                 name: 'Let\'s Eat Hero',
-                link: 'https://pp.grubhub.com/lets-eat/?chiri_homepage-hero-content=',
-                id: '8'
+                link: 'https://pp.grubhub.com/lets-eat/?chiri_homepage-hero-content='
             },
             {
                 name: 'Let\'s Eat Top',
-                link: 'https://pp.grubhub.com/lets-eat/?chiri_homepage-top-content=',
-                id: '9'
+                link: 'https://pp.grubhub.com/lets-eat/?chiri_homepage-top-content='
             },
             {
                 name: 'Let\'s Eat Middle',
-                link: 'https://pp.grubhub.com/lets-eat/?chiri_homepage-middle-content=',
-                id: '10'
+                link: 'https://pp.grubhub.com/lets-eat/?chiri_homepage-middle-content='
             },
             {
                 name: 'Let\'s Eat Footer',
-                link: 'https://pp.grubhub.com/lets-eat/?chiri_homepage-footer-content=',
-                id: '11'
+                link: 'https://pp.grubhub.com/lets-eat/?chiri_homepage-footer-content='
             },
             {
                 name: 'Let\'s Eat Subfooter',
-                link: 'https://pp.grubhub.com/lets-eat/?chiri_homepage-subfooter-content=',
-                id: '12'
+                link: 'https://pp.grubhub.com/lets-eat/?chiri_homepage-subfooter-content='
             },
             {
                 name: 'Search',
-                link: 'https://pp.grubhub.com/search?orderMethod=delivery&locationMode=DELIVERY&facetSet=umamiV2&pageSize=20&hideHateos=true&searchMetrics=true&latitude=37.78666305&longitude=-122.39997101&facet=open_now%3Atrue&variationId=default-impressionScoreViewAdjSearchOnlyBuffed-20160607&sortSetId=umamiV2&sponsoredSize=3&countOmittingTimes=true?chiri_search-banner=',
-                id: '13'
+                link: 'https://pp.grubhub.com/search?orderMethod=delivery&locationMode=DELIVERY&facetSet=umamiV2&pageSize=20&hideHateos=true&searchMetrics=true&latitude=37.78666305&longitude=-122.39997101&facet=open_now%3Atrue&variationId=default-impressionScoreViewAdjSearchOnlyBuffed-20160607&sortSetId=umamiV2&sponsoredSize=3&countOmittingTimes=true?chiri_search-banner='
             },
             {
                 name: 'Restaurant',
-                link: 'https://pp.grubhub.com/restaurant/india-clay-oven-restaurant-and-full-bar-2436-clement-st-san-francisco/54117?chiri_restaurant-banner=',
-                id: '14'
+                link: 'https://pp.grubhub.com/restaurant/india-clay-oven-restaurant-and-full-bar-2436-clement-st-san-francisco/54117?chiri_restaurant-banner='
             }
         ];
     }
