725641653e595a15c9faac854723655a945b3673
Trung Le <trung.t.le.nguyen@gmail.com>
2018-03-02 08:45:29 -0800

    Made various changes to make the tool easier to update in the future.

--- app/contentfulController.js
+++ app/contentfulController.js
@@ -5,63 +5,125 @@ app.controller('contentfulController', [
             title: 'Contentful Frontend Testing'
         };
 
-        $scope.model.placements = [
+        $scope.model.homepage = [
             {
-                name: 'Homepage Container',
+                name: 'Container',
                 link: 'https://pp.grubhub.com/?chiri_homepage-container='
             },
             {
-                name: 'Homepage Hero',
+                name: 'Hero',
                 link: 'https://pp.grubhub.com/?chiri_homepage-hero-content='
             },
             {
-                name: 'Homepage Top',
+                name: 'Top',
                 link: 'https://pp.grubhub.com/?chiri_homepage-top-content='
             },
             {
-                name: 'Homepage Middle',
+                name: 'Middle',
                 link: 'https://pp.grubhub.com/?chiri_homepage-middle-content='
             },
             {
-                name: 'Homepage Footer',
+                name: 'Footer',
                 link: 'https://pp.grubhub.com/?chiri_homepage-footer-content='
             },
             {
-                name: 'Homepage Subfooter',
+                name: 'Subfooter',
                 link: 'https://pp.grubhub.com/?chiri_homepage-subfooter-content='
-            },
+            }
+        ];
+
+        $scope.model.letsEat = [
             {
-                name: 'Let\'s Eat Container',
+                name: 'Container',
                 link: 'https://pp.grubhub.com/lets-eat/?chiri_homepage-container='
             },
             {
-                name: 'Let\'s Eat Hero',
+                name: 'Hero',
                 link: 'https://pp.grubhub.com/lets-eat/?chiri_homepage-hero-content='
             },
             {
-                name: 'Let\'s Eat Top',
+                name: 'Top',
                 link: 'https://pp.grubhub.com/lets-eat/?chiri_homepage-top-content='
             },
             {
-                name: 'Let\'s Eat Middle',
+                name: 'Middle',
                 link: 'https://pp.grubhub.com/lets-eat/?chiri_homepage-middle-content='
             },
             {
-                name: 'Let\'s Eat Footer',
+                name: 'Footer',
                 link: 'https://pp.grubhub.com/lets-eat/?chiri_homepage-footer-content='
             },
             {
-                name: 'Let\'s Eat Subfooter',
+                name: 'Subfooter',
                 link: 'https://pp.grubhub.com/lets-eat/?chiri_homepage-subfooter-content='
-            },
+            }
+        ];
+
+        $scope.model.search = [
             {
-                name: 'Search',
+                name: 'Top',
                 link: 'https://pp.grubhub.com/search?orderMethod=delivery&locationMode=DELIVERY&facetSet=umamiV2&pageSize=20&hideHateos=true&searchMetrics=true&latitude=37.78666305&longitude=-122.39997101&facet=open_now%3Atrue&variationId=default-impressionScoreViewAdjSearchOnlyBuffed-20160607&sortSetId=umamiV2&sponsoredSize=3&countOmittingTimes=true?chiri_search-banner='
-            },
+            }
+        ];
+
+        $scope.model.restaurant = [
             {
-                name: 'Restaurant',
+                name: 'Top',
                 link: 'https://pp.grubhub.com/restaurant/india-clay-oven-restaurant-and-full-bar-2436-clement-st-san-francisco/54117?chiri_restaurant-banner='
             }
+        ];
+
+        $scope.model.orderStatus = [
+            {
+                    name: 'Container',
+                    link: 'https://pp.grubhub.com/'
+            },
+            {
+                    name: 'Top',
+                    link: 'https://pp.grubhub.com/'
+            },
+            {
+                    name: 'Footer',
+                    link: 'https://pp.grubhub.com/'
+            },
+            {
+                    name: 'Receipt',
+                    link: 'https://pp.grubhub.com/'
+            }
+        ];
+
+        $scope.model.orderReceipt = [
+            {
+                name: 'Container',
+                link: 'https://pp.grubhub.com/'
+            },
+            {
+                name: 'Top',
+                link: 'https://pp.grubhub.com/'
+            },
+            {
+                name: 'Receipt',
+                link: 'https://pp.grubhub.com/'
+            }
+        ];
+
+        $scope.model.thankYou = [
+            {
+                name: 'Container',
+                link: 'https://pp.grubhub.com/'
+            },
+            {
+                name: 'Top',
+                link: 'https://pp.grubhub.com/'
+            },
+            {
+                name: 'Footer',
+                link: 'https://pp.grubhub.com/'
+            },
+            {
+                name: 'Receipt',
+                link: 'https://pp.grubhub.com/'
+            },
         ];
     }
 ]);
